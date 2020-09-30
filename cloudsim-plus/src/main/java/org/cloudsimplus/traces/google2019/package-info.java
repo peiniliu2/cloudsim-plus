22f5fd869bfeb5bdf80c0b7f252a97ca382d2d1f
peiniliu <peini.liu@gmail.com>
2020-09-30 16:19:50 +0200

    create google2019
    Signed-off-by: peiniliu <peini.liu@gmail.com>

---
+++ cloudsim-plus/src/main/java/org/cloudsimplus/traces/google2019/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * https://drive.google.com/file/d/10r6cnJ5cJ89fPWCgj7j4LtLBqYN9RiI9/view
+ * 
+ * @author Peini Liu
+ *
+ * Machines [Machine events, Machine attributes]
+ * Collections and instances [CollectionEvents, InstanceEvents(machine constraint)]
+ * Resource usage [InstanceUsage]
+ *
+ * @see org.cloudsimplus.traces.google.GoogleMachineEventsTraceReader
+ * @see org.cloudsimplus.traces.google.GoogleTaskEventsTraceReader
+ * @see org.cloudsimplus.traces.google.GoogleTaskUsageTraceReader
+ */
+package org.cloudsimplus.traces.google2019;
